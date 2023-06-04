30f5c7ceb4e5f0d9b9d680cf68de29c6547fb4e2
aqilikhwan21 <aqil_ikhwan@yahoo.com>
2023-06-04 17:32:08 +0900

    (+) View Profile and User Input

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class Challenge {
 
   Challenge({required this.category, required this.description});
 }
-
+final List<String> userInputList = [];
 List<Challenge> weightLossChallenges = [
   Challenge(category: 'Weight Loss', description: 'Take a 30-minute brisk walk today.'),
   Challenge(category: 'Weight Loss', description: 'Replace sugary drinks with water for the entire day.'),
@@ -38,6 +38,9 @@ Challenge getChallengeBasedOnTime(List<Challenge> challenges) {
   return selectedChallenge;
 }
 void main() {
+  final weightLossChallenge = getChallengeBasedOnTime(weightLossChallenges);
+  final healthyEatingChallenge = getChallengeBasedOnTime(healthyEatingChallenges);
+  final healthyLifestyleChallenge = getChallengeBasedOnTime(healthyLifestyleChallenges);
   // Generate challenges based on the current time of the day
   runApp(MyApp());
 }
@@ -54,6 +57,7 @@ class MyApp extends StatelessWidget {
         '/': (context) => MainPage(), // Set the main page as the home
         SignInPage.routeName: (context) => SignInPage(),
         ProfilePage.routeName: (context) => ProfilePage(),
+        View_ProfilePage.routeName: (context) => View_ProfilePage(userInputList: [],)
       },
     );
   }
@@ -288,6 +292,7 @@ class SignInPage extends StatelessWidget {
 }
 
 class ProfilePage extends StatelessWidget {
+  List<String> userInputList = List<String>.filled(5, '');
   final weightLossChallenge = getChallengeBasedOnTime(weightLossChallenges);
   final healthyEatingChallenge = getChallengeBasedOnTime(healthyEatingChallenges);
   final healthyLifestyleChallenge = getChallengeBasedOnTime(healthyLifestyleChallenges);
@@ -331,12 +336,12 @@ class ProfilePage extends StatelessWidget {
               child : Column(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  _personalDetailInfo(weightLossChallenge.description,"assets/Icon awesome.png"),
-                  _personalDetailInfo("24 Oct 2000", "assets/cake.png"),
-                  _personalDetailInfo("010-7426-3239", "assets/phone.png"),
-                  _personalDetailInfo("@DavidKim42", "assets/instagram.png"),
-                  _personalDetailInfo("davidkim@gmail.com", "assets/mail.png"),
-                  _personalDataInfoButton(),
+                  _personalDetailInfo(0,  "assets/Icon awesome.png"),
+                  _personalDetailInfo(1,  "assets/cake.png"),
+                  _personalDetailInfo(2,  "assets/phone.png"),
+                  _personalDetailInfo(3,  "assets/instagram.png"),
+                  _personalDetailInfo(4,  "assets/mail.png"),
+                  _personalDataInfoButton(context, userInputList),
                 ],
               )
             ),
@@ -413,42 +418,293 @@ class ProfilePage extends StatelessWidget {
     );
   }
 
+  Widget _personalDetailInfo(int index, String imagePath) {
+    TextEditingController controller = TextEditingController();
+    String shadowText = '';
+    switch (index) {
+      case 0:
+        shadowText = 'Enter Your Name';
+        break;
+      case 1:
+        shadowText = 'Enter your birthdate';
+        break;
+      case 2:
+        shadowText = 'Enter your phone number';
+        break;
+      case 3:
+        shadowText = 'Enter your Instagram username';
+        break;
+      case 4:
+        shadowText = 'Enter your email address';
+        break;
+    }
+
+    return Container(
+      margin: EdgeInsets.only(bottom: 10),
+      child: Row(
+        children: [
+          Container(
+            width: 24,
+            height: 24,
+            child: Image.asset(imagePath),
+          ),
+          SizedBox(width: 8),
+          Expanded(
+            child: TextFormField(
+              controller: controller,
+              onChanged: (value) {
+                userInputList[index] = value;
+              },
+              decoration: InputDecoration(
+                contentPadding:
+                EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+                enabledBorder: const OutlineInputBorder(
+                  borderSide: BorderSide(
+                    width: 1,
+                    color: Colors.lightGreen,
+                  ),
+                  borderRadius: BorderRadius.all(Radius.circular(5)),
+                ),
+                focusedBorder: const OutlineInputBorder(
+                  borderSide: BorderSide(
+                    width: 1,
+                    color: Colors.green,
+                  ),
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                ),
+                hintText: shadowText,
+                hintStyle: TextStyle(
+                  color: Colors.grey,
+                  fontStyle: FontStyle.italic,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  ElevatedButton _personalDataInfoButton(BuildContext context, List<String> userInputList) {
+    return ElevatedButton(
+      style: ButtonStyle(
+        foregroundColor: MaterialStatePropertyAll(Colors.white),
+        backgroundColor: MaterialStatePropertyAll(Colors.green.shade300),
+        fixedSize: MaterialStatePropertyAll(Size(160, 50)),
+        elevation: MaterialStatePropertyAll(3),
+      ),
+      onPressed: () {
+        // Navigate to another page when the button is pressed
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) => View_ProfilePage(userInputList : userInputList)
+          ),
+        );
+      },
+      child: const Text(
+        "SAVE",
+        style: TextStyle(
+          fontSize: 17,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+
+  void _saveUserInput(BuildContext context) {
+    // Perform any validation or additional processing here before saving the input
+
+    // Display the user inputs in a dialog
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('User Inputs'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: List.generate(
+              userInputList.length,
+                  (index) => Text('${index + 1}. ${userInputList[index]}'),
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text('OK'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
+
+class View_ProfilePage extends StatelessWidget {
+  final List<String> userInputList;
+
+  View_ProfilePage({required this.userInputList});
+  static const routeName = '/profile';
+  @override
+  Widget build(BuildContext context) {
+
+    Widget getInfoSection = Card(
+      margin: const EdgeInsets.fromLTRB(15, 0, 15, 0),
+      elevation: 0,
+      child: Column(
+        // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Expanded(
+              flex: 1,
+              child: Column(
+                children: [
+                  Expanded(
+                    flex: 5,
+                    child: CircleAvatar(
+                      backgroundImage: AssetImage('assets/avatar1.png'),
+                      radius: 60,
+                    ),
+                  ),
+                  Expanded(
+                      flex: 2,
+                      child: Text(
+                        "Welcome To Profile Page",
+                        style: TextStyle(
+                          color: Colors.green.shade500,
+                          fontSize: 20,
+                        ),
+                      )
+                  )
+                ],
+              )
+          ),
+          Expanded(
+            flex: 2,
+            child: SingleChildScrollView(
+                child : Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    _personalDetailInfo( userInputList[0],"assets/Icon awesome.png"),
+                    _personalDetailInfo(userInputList[1], "assets/cake.png"),
+                    _personalDetailInfo(userInputList[2], "assets/phone.png"),
+                    _personalDetailInfo(userInputList[3], "assets/instagram.png"),
+                    _personalDetailInfo(userInputList[4], "assets/mail.png"),
+                    _personalDataInfoButton(),
+                  ],
+                )
+            ),
+          )
+        ],
+      ),
+    );
+
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: SystemUiOverlayStyle.dark,
+      child : Stack (
+        children : [
+          Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    colors: [ Colors.white, Colors.green.shade200 ]
+                ),
+              ),
+              child: Column(
+                children: [
+                  Expanded(
+                      flex: 2,
+                      child: Container(
+                          decoration: const BoxDecoration(
+                              image: DecorationImage(
+                                  image: AssetImage('assets/robot-lookDown.png'),
+                                  fit: BoxFit.cover
+                              )
+                          )
+                      )
+                  ),
+                  Expanded(
+                    flex: 10,
+                    child: Container(
+                      margin: const EdgeInsets.fromLTRB(20, 0, 20, 50),
+                      height: double.infinity,
+                      width: double.infinity,
+                      decoration: const BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.all(Radius.circular(8)),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey,
+                              spreadRadius: 1,
+                              blurRadius: 3,
+                              offset: Offset(0, 3),
+                            )
+                          ]
+                      ),
+                      child: getInfoSection,
+                    ),
+                  )
+                ],
+              )
+          ),
+          Positioned(
+            top: 48,
+            right: 8,
+            child: GestureDetector(
+              onTap: () {
+                Navigator.pop(context);
+              },
+              child: Image.asset(
+                'assets/back_btn.png',
+                width: 60,
+                height: 60,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
   Widget _personalDetailInfo(String value, String imagePath) {
     return Container(
       margin: EdgeInsets.only(bottom: 10), // Add margin of 8 pixels at the bottom
       child: Row(
-          children: [
-            Container(
-              width: 24,
-              height: 24,
-              child : Image.asset(imagePath), // Image widget
-            ),
-            SizedBox(width: 8),
-            Expanded(
-              child : TextFormField(// Add a space between the image and the TextFormField
+        children: [
+          Container(
+            width: 24,
+            height: 24,
+            child : Image.asset(imagePath), // Image widget
+          ),
+          SizedBox(width: 8),
+          Expanded(
+            child : TextFormField(// Add a space between the image and the TextFormField
               readOnly: true,
               initialValue: value,
               decoration: InputDecoration(
                 contentPadding: EdgeInsets.symmetric(vertical: 12, horizontal: 16),
                 enabledBorder: const OutlineInputBorder(
-                  borderSide: BorderSide(
-                    width: 1, color: Colors.lightGreen,
+                    borderSide: BorderSide(
+                      width: 1, color: Colors.lightGreen,
                     ),
-                  borderRadius: BorderRadius.all(Radius.circular(5))
-                    ),
+                    borderRadius: BorderRadius.all(Radius.circular(5))
+                ),
                 focusedBorder: const OutlineInputBorder(
-                  borderSide: BorderSide(
-                    width: 1, color: Colors.green,
+                    borderSide: BorderSide(
+                      width: 1, color: Colors.green,
                     ),
-                  borderRadius: BorderRadius.all(Radius.circular(10))
-                  ),
+                    borderRadius: BorderRadius.all(Radius.circular(10))
                 ),
               ),
             ),
-          ],
-        ),
-      );
-    }
+          ),
+        ],
+      ),
+    );
+  }
 
   ElevatedButton _personalDataInfoButton() {
     return ElevatedButton(
